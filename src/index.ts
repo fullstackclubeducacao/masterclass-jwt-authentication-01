f50b5cdc5616eaae12dad19b7421dc6cfa33ee1f
Felipe Rocha <felipemota.rocha@gmail.com>
2024-12-03 18:37:23 -0300

    add tokens generation on register

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 import "./database";
 import { UserModel } from "./database";
 import { isEmailValid, isPasswordValid } from "./helpers";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.use(express.json());
 
 app.post("/register", async (req, res) => {
   try {
+    if (!process.env.JWT_SECRET || !process.env.JWT_REFRESH_SECRET) {
+      throw new Error("Missing JWT_SECRET or JWT_REFRESH_SECRET in .env file");
+    }
     const { email, password } = req.body;
     if (!isEmailValid(email)) {
       res.status(400).json({
@@ -26,11 +30,21 @@ app.post("/register", async (req, res) => {
     //   criptografar a minha senha
     const hashedPassword = bcrypt.hashSync(password, 10);
     // preciso salvar isso no meu banco de dados
-    await UserModel.create({
+    const user = await UserModel.create({
       email,
       password: hashedPassword,
     });
-    res.status(201).json({ email });
+    const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+    const refreshToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_REFRESH_SECRET,
+      {
+        expiresIn: "30d",
+      }
+    );
+    res.status(201).json({ email, tokens: { accessToken, refreshToken } });
   } catch (error) {
     res.status(500).json({
       message: "Internal server error",
